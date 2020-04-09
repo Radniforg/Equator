f3d8c6819210c654cb73124e8156d9d486d3e1b9
Ivan Alexandrov <radniforg@yandex.ru>
2020-04-09 15:43:30 +0300

    PY script polish.py modified

--- polish.py
+++ polish.py
@@ -1,25 +1,23 @@
-
 polish = input('Пожалуйста, введите желаемую арифметическую операцию в формате: "Знак Число1 Число2"\n')
 
 sequence_processed = polish.split(' ')
-if len(polish.split(' ')) != 3:
-  print('Ошибка. Убедитесь, что знаки и числа разделены пробелами')
-else:
-  try:
+assert len(polish.split(' ')) == 3, 'Ошибка. Убедитесь, что знаки и числа разделены пробелами'
+assert sequence_processed[0] in ['+', '-', '*', '/'], 'Ошибка: задана не арифметическая операция'
+
+try:
+  if int(sequence_processed[1]) >= 0 and int(sequence_processed[2]) >= 0:
     if sequence_processed[0] == '+':
       arithmetic = int(sequence_processed[1]) + int(sequence_processed[2])
     elif sequence_processed[0] == '-':
       arithmetic = int(sequence_processed[1]) - int(sequence_processed[2])
     elif sequence_processed[0] == '*':
       arithmetic = int(sequence_processed[1]) * int(sequence_processed[2])
-    elif sequence_processed[0] == '/':
+    else:
       arithmetic = int(sequence_processed[1]) / int(sequence_processed[2])
-    else: 
-      arithmetic = 'Ошибка: задана не арифметическая операция'
-  except ValueError:
-    arithmetic = 'Ошибка: задано не число'
-  except ZeroDivisionError:
-    arithmetic = 0
-    print ('Внимание: деление на ноль!')
+except ValueError:
+  arithmetic = 'Ошибка ввода: задано не положительное число'
+except ZeroDivisionError:
+  arithmetic = 'Внимание: деление на ноль!'
+except Exception:
+  arithmetic = 'Непредвиденная ошибка'
 print(arithmetic)
-  
